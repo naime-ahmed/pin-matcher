25e0f526cf292f1b485766442b91dee3064b7642
naime-molla <naime2molla@gmail.com>
2021-09-15 19:13:45 +0600

    first commit

---
+++ js/test.js
@@ -0,0 +1,19 @@
+/* function generatePin() {
+    return Math.floor(Math.random() * 90000) ;
+}
+// console.log(generatePin());
+
+if (!isNaN('10X') == true) {
+    console.log(true);
+}
+else {
+    console.log(false);
+} */
+
+const product = 5;
+const price = '7';
+const subTotal = product * price;
+// console.log(subTotal);
+const tax = subTotal / 10;
+const total = subTotal + tax;
+console.log(total);
